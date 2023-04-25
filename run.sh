0f930e5ab87d69c179618d513c66d88bc1d1f9b9
Ooohv <129358695+Ooohv@users.noreply.github.com>
2023-04-25 10:53:22 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+echo "一键搭建脚本"
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O replit.nix https://github.com/Ooohv/-replit/blob/main/replit.nix
+cd ..
+wget -O main.sh https://github.com/sxbai/alist-on-replit/raw/master/main.sh
+cp -r build/replit.nix .
+echo "恭喜搭建完成"
+echo "点击Run立即运行项目"
+echo "查询用户名及密码运行下面命令"
+echo "./sx admin"
+rm -rf build/
