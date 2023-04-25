fd889a03e9ff609a828fb01bd1ebf06f15c97fa1
Ooohv <129358695+Ooohv@users.noreply.github.com>
2023-04-25 12:16:45 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
-echo "一键搭建脚本"
-wget -O replit.nix https://github.com/Ooohv/-replit/blob/main/replit.nix
+echo "Alist一键搭建脚本"
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://github.com/sxbai/alist-on-replit/raw/master/.replit
+wget -O replit.nix https://github.com/sxbai/alist-on-replit/raw/master/replit.nix
+cd ..
 wget -O main.sh https://github.com/sxbai/alist-on-replit/raw/master/main.sh
+wget -O sx https://down.sxbai.repl.co/alist/v3.15.0/alist3.15.0
+cp -r build/.replit . && cp -r build/replit.nix .
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
 echo "查询用户名及密码运行下面命令"
 echo "./sx admin"
+rm -rf build/
