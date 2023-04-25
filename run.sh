ba5f83261b4d7264b5f995cacdab0254a76101aa
Ooohv <129358695+Ooohv@users.noreply.github.com>
2023-04-25 15:04:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ echo "一键搭建脚本"
 nix-env -iA nixpkgs.wget
 mkdir build
 cd build
-wget -O replit.nix 
+wget -O replit.nix https://github.com/Ooohv/-replit/blob/main/replit.nix
 cd ..
-wget -O main.sh 
+wget -O main.sh https://github.com/Ooohv/-replit/blob/main/main.sh
 cp -r build/replit.nix .
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
