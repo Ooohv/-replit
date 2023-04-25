88930b0297c949bd83cf82e85e588331d6bbdc00
Ooohv <129358695+Ooohv@users.noreply.github.com>
2023-04-25 14:24:00 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-echo "Alist一键搭建脚本"
-nix-env -iA nixpkgs.wget
-mkdir build
-cd build
-wget -O .replit https://github.com/Ooohv/-replit/blob/main/.replit
-wget -O replit.nix https://github.com/Ooohv/-replit/blob/main/replit.nix
-cp -r build/.replit . && cp -r build/replit.nix .
-rm -rf build/
