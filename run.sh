9f29182d0660dc7e38eaf6abeccb2a3ae3acb149
Ooohv <129358695+Ooohv@users.noreply.github.com>
2023-04-25 12:32:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,5 @@ mkdir build
 cd build
 wget -O .replit https://github.com/Ooohv/-replit/blob/main/.replit
 wget -O replit.nix https://github.com/Ooohv/-replit/blob/main/replit.nix
-cd ..
-wget -O main.sh https://github.com/Ooohv/-replit/blob/main/main.sh
 cp -r build/.replit . && cp -r build/replit.nix .
-echo "恭喜搭建完成"
-echo "点击Run立即运行项目"
-echo "查询用户名及密码运行下面命令"
-echo "./sx admin"
 rm -rf build/
